c07dd31fd1ec35d0217711b04727a0fc0f46d759
Ivan <ivancardozo11@hotmail.com>
2018-04-19 22:58:44 -0300

    uploading , todo andando bien .

--- public/app.js
+++ public/app.js
@@ -1,12 +1,14 @@
-"use strict";
-
 var $ = window.jQuery;
 var MarvelApi = window.MarvelApi;
 
 var key = '19b62fcb015fef703d87076b400c365c';
 var api = new MarvelApi(key)
 
-api.findSeries('avengers').then(function (serie) {
+  api.findSeries('avengers').then(function (serie) {
+  var serieImage = serie.thumbnail.path + "." + serie.thumbnail.extension;
+  $(".layout").css("backgroundImage", serieImage);
+  debugger;
+
   var characters = serie.characters.items;
   var promises = [];
   var _iteratorNormalCompletion = true;
